2f351ba4a5e8c7513d565024db6455a5e1339b87
Tony Martinez <ajosephmartinez@gmail.com>
2020-10-27 10:55:58 -0700

    .

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,16 @@
-import React from 'react';
-import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
-import importedComponent from 'react-imported-component';
+import React from "react";
+import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
+import importedComponent from "react-imported-component";
 
-import Home from './Home';
-import Loading from './Loading';
-
-const AsyncDynamicPAge = importedComponent(
-  () => import(/* webpackChunkName:'DynamicPage' */ './DynamicPage'),
-  {
-    LoadingComponent: Loading
-  }
-);
-const AsyncNoMatch = importedComponent(
-  () => import(/* webpackChunkName:'NoMatch' */ './NoMatch'),
-  {
-    LoadingComponent: Loading
-  }
-);
+import Loading from "./Loading";
+import { Button } from "@chakra-ui/core";
+import "focus-visible/dist/focus-visible";
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/dynamic" component={AsyncDynamicPAge} />
-          <Route component={AsyncNoMatch} />
-        </Switch>
-      </div>
-    </Router>
+    <div style={{ margin: "5em" }}>
+      <Button size="lg">Click me</Button>
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import { hot } from "react-hot-loader/root";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { ChakraProvider } from "@chakra-ui/core";
 
 import "./index.css";
 
 const render = (Component) =>
-  ReactDOM.render(<Component />, document.getElementById("root"));
+  ReactDOM.render(
+    <ChakraProvider resetCSS={false}>
+      <Component />
+    </ChakraProvider>,
+    document.getElementById("root")
+  );
 
 render(hot(App));
